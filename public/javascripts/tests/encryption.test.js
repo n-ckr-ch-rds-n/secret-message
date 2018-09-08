af6b3af28a61040b99fed86c6fbd6dd4218333bb
Nick <nicholasjohnrichardson@gmail.com>
2018-09-08 20:59:41 +0100

    fixed unit tests

--- public/javascripts/tests/encryption.test.js
+++ public/javascripts/tests/encryption.test.js
@@ -1,20 +1,21 @@
 const Encryption = require('../encryption.js');
 
-const encryption = new Encryption();
-
 describe('Encryption', function() {
   it('encrypts and decrypts default message', function() {
+    const encryption = new Encryption();
     let encrypted = encryption.encrypt();
     expect(encryption.decrypt(encrypted, 'Shamash')).toEqual('He who controls the spice controls the universe');
   })
 
   it('can be passed different messages and keys', function() {
-    let encrypted = encryption.encrypt('message', 'key');
+    const encryption = new Encryption('message', 'key');
+    let encrypted = encryption.encrypt();
     expect(encryption.decrypt(encrypted, 'key')).toEqual('message');
   })
 
   it('does not decrypt if passed the wrong key', function() {
-    let encrypted = encryption.encrypt('message', 'key');
+    const encryption = new Encryption();
+    let encrypted = encryption.encrypt();
     expect(encryption.decrypt(encrypted, 'wrongkey')).toEqual('');
   })
 })
