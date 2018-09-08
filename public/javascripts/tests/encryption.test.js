1371f0f3a3ec4dab1c72c571a87eb1403b03a1ae
Nick <nicholasjohnrichardson@gmail.com>
2018-09-08 20:20:27 +0100

    added two more unit tests for encryption module

--- public/javascripts/tests/encryption.test.js
+++ public/javascripts/tests/encryption.test.js
@@ -7,4 +7,14 @@ describe('Encryption', function() {
     let encrypted = encryption.encrypt();
     expect(encryption.decrypt(encrypted, 'Shamash')).toEqual('He who controls the spice controls the universe');
   })
+
+  it('can be passed different messages and keys', function() {
+    let encrypted = encryption.encrypt('message', 'key');
+    expect(encryption.decrypt(encrypted, 'key')).toEqual('message');
+  })
+
+  it('does not decrypt if passed the wrong key', function() {
+    let encrypted = encryption.encrypt('message', 'key');
+    expect(encryption.decrypt(encrypted, 'wrongkey')).toEqual('');
+  })
 })
